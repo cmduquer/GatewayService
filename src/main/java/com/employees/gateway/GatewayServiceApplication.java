f8b47d36c44b8c2c3f4226704dfb209a86ef7b58
cduque <cduque@avanxo.com>
2019-05-31 16:12:25 -0500

    https://github.com/cmduquer/GatewayService.git

---
+++ src/main/java/com/employees/gateway/GatewayServiceApplication.java
@@ -0,0 +1,19 @@
+package com.employees.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableHystrix
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
