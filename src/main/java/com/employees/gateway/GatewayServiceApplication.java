11e554c04f48f6fa2471961901acf998da5035b1
cmduquer <cmduquer@192.168.1.4>
2019-06-14 16:04:39 -0500

    kubernetes

--- src/main/java/com/employees/gateway/GatewayServiceApplication.java
+++ src/main/java/com/employees/gateway/GatewayServiceApplication.java
@@ -2,14 +2,15 @@ package com.employees.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableZuulProxy
-@EnableEurekaClient
-@EnableHystrix
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
